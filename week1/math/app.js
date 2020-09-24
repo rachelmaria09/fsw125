807ab301420dec4a9b8d945edee6270b13f08e97
rachelmaria09 <rmgildea@gmail.com>
2020-09-24 16:48:25 -0400

    first

---
+++ week1/math/app.js
@@ -0,0 +1,6 @@
+const { sum, subtract, multiply, divide } = require ("./math")
+
+console.log(sum(2, 2)) //4
+console.log(subtract(4, 2)) //2
+console.log(multiply(4, 2)) //8
+console.log(divide(4, 2)) //2
