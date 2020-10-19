b1da7f0685b2e039b303b1c2f2672ea33471fdfa
rachelmaria09 <rmgildea@gmail.com>
2020-10-19 14:50:46 -0400

    update

---
+++ week4/routes/bountyRouter.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bountyRouter = express.Router()
+const uuid = require("uuid")
+
+const bounties = [
+    { firstName: "Darth", lastName: "Vader", living: true, type: "Sith", _id: uuid.v4() },
+    { firstName: "Luke", lastName: "Skywalker", living: true, type: "Jedi", _id: uuid.v4() },
+    { firstName: "Obi-Wan", lastName: "Kenobi", living: false, type: "Jedi", _id: uuid.v4() },
+    { firstName: "Sheeve", lastName: "Palpatine", living: false, type: "Sith", _id: uuid.v4() },
+    { firstName: "Princess", lastName: "Leia", living: true, type: "Jedi", _id: uuid.v4() },
+]
+
+bountyRouter.route("/")
+.get((req, res) => {
+    res.send(bounties)
+})
+.post((req, res) => {
+    const newBounty = req.body
+    newBounty._id = uuid()
+    bounties.push(newBounty)
+    res.send(`New bounty added to the list: ${newBounty.firstName} !`)
+})
+
+bountyRouter.get("/:bountyId", (req, res) => {
+    const bountyId = req.params.bountyId
+    const foundBounty = bounties.find(bounty => bounty._id === bountyId)
+    res.send(foundBounty)
+})
+
+module.exports = bountyRouter
