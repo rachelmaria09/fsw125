6c8dbacaa0bc428688910f3fabaf64af6766dd0f
rachelmaria09 <rmgildea@gmail.com>
2020-10-21 12:47:19 -0400

    update

--- week4/routes/bountyRouter.js
+++ week4/routes/bountyRouter.js
@@ -11,20 +11,36 @@ const bounties = [
 ]
 
 bountyRouter.route("/")
+//get all
 .get((req, res) => {
     res.send(bounties)
 })
+//post one
 .post((req, res) => {
     const newBounty = req.body
     newBounty._id = uuid()
     bounties.push(newBounty)
     res.send(`New bounty added to the list: ${newBounty.firstName} !`)
 })
-
+//get one
 bountyRouter.get("/:bountyId", (req, res) => {
     const bountyId = req.params.bountyId
     const foundBounty = bounties.find(bounty => bounty._id === bountyId)
     res.send(foundBounty)
 })
-
+//delete one
+bountyRouter.delete("/:bountyId", (req, res) => {
+    const bountyId = req.params.bountyId
+    const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+    bounties.splice(bountyIndex, 1)
+    res.send("Successfully killed a bounty!") 
+})
+//update one
+bountyRouter.put("/:bountyId", (req, res) => {
+    const bountyId = req.params.bountyId
+    const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+    const updatedBounty = Object.assign(bounties[bountyIndex], req.body)
+    console.log(updatedBounty)
+    res.send(updatedBounty)
+})
 module.exports = bountyRouter
