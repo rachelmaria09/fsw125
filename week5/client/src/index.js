4a2710ce3e6770cac7b1e23bc7ba6b884963d7b7
rachelmaria09 <rmgildea@gmail.com>
2020-10-26 13:05:39 -0400

    first

---
+++ week5/client/src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// axios.get("/bounties")
+
+ReactDOM.render(<h2>Hello World</h2>, document.getElementById("root"))
---
+++ week5/routes/server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+app.use("/bounties", require("./routes/bountyRouter.js"))
+
+app.listen(9000, () => {
+    console.log("Server is working!")
+})
