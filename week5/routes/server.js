74f8e0fa16d613b153131582a5178bf7c8e60226
rachelmaria09 <rmgildea@gmail.com>
2020-11-02 11:09:25 -0500

    update

--- week5/routes/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const app = express()
-
-app.use(express.json())
-app.use("/bounties", require("./routes/bountyRouter.js"))
-
-app.listen(9000, () => {
-    console.log("Server is working!")
-})
