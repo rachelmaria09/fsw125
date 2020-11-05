c56c5fe1a932b2d4d653b8970b3391ff554f795c
rachelmaria09 <rmgildea@gmail.com>
2020-11-05 17:31:06 -0500

    final

--- week5/server.js
+++ week5/server.js
@@ -7,21 +7,6 @@ app.use(morgan('dev'))
 
 app.use("/bounties", require('./bountyRouter'))
 
-// app.use('/bounties', (req, res, next) => {
-//     console.log('Bounties middleware was executed!')
-//     next()
-// })
-
-// app.use('/bounties', (req, res, next) => {
-//     req.body = {name: "Rachel"}
-//     next()
-// })
-
-// app.get('/bounties', (req, res, next) => {
-//     console.log('Get request received!')
-//     res.send(req.body)
-// })
-
 app.listen(9000, () => {
     console.log("Server is working!")
 })
